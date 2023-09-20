b85049f1ddcae1ec163c92773b8d51e4f44ec09b
G-Brandmaier <brandmaier.gabriella@gmail.com>
2023-09-20 15:14:46 +0200

    Add tcpclient and listener for client_side and server_side

--- Uppgift_1/Client_Side/Client_Side/Program.cs
+++ Uppgift_1/Client_Side/Client_Side/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Net.Sockets;
+﻿using Client_Side.Classes;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Xml.Linq;
 
 namespace Client_Side;
 
@@ -7,5 +11,43 @@ internal class Program
     static void Main(string[] args)
     {
 
+        Weather weather = new ("Cloudy and 50% chance of rain", 16, 10, DateTime.Now.Date.ToShortDateString(), DateTime.Now.ToShortTimeString());
+
+        string jsonStringToSend = JsonSerializer.Serialize(weather);
+
+        Connect(jsonStringToSend);
+    }
+
+    static void Connect(string productInformation)
+    {
+        try
+        {
+            var port = 8080;
+            using TcpClient client = new TcpClient("127.0.0.1", port);
+
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(productInformation);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine($"Sent to server: {productInformation}\n");
+
+            data = new byte[256];
+
+            string response = string.Empty;
+
+            var bytes = stream.Read(data, 0, data.Length);
+            response = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine($"Received from server: {response}\n");
+
+            Console.ReadKey();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to connect");
+            Console.WriteLine(ex.Message);
+        }
     }
 }
