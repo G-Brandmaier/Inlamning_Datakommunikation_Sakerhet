09a52bfc333c008a10b3355587bbbbc9d17a2d94
G-Brandmaier <brandmaier.gabriella@gmail.com>
2023-09-23 11:30:19 +0200

    Add client and listener for assignment 2

--- Uppgift_2/Udp_Client_1/Udp_Client_1/Program.cs
+++ Uppgift_2/Udp_Client_1/Udp_Client_1/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Udp_Client_1
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using Udp_Client.Classes;
+
+namespace Udp_Client;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        WeatherData weather = new("Cloudy and 50% chance of rain", 16, 10, DateTime.Now.Date.ToShortDateString(), DateTime.Now.ToShortTimeString());
+
+        string jsonStringToSend = JsonSerializer.Serialize(weather);
+
+        SendMessage(jsonStringToSend);
+    }
+
+    private static void SendMessage(string data)
+    {
+        UdpClient udpClient = new UdpClient();
+        try
         {
-            Console.WriteLine("Hello, World!");
+            udpClient.Connect("127.0.0.1", 8080);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+
+            udpClient.Send(bytes, bytes.Length);
+
+            udpClient.Close();
+
+            Console.WriteLine("Data was sent");
+            Console.ReadKey();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Something went wrong!");
+            Console.WriteLine(ex.Message);
+
         }
     }
 }
